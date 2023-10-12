4b33bd789bd72bf69f51766fb642094c696cb831
writtenbyrla <gnnvistavie@gmail.com>
2023-10-12 17:50:25 +0900

    풀캘린더 수정

--- fullcalendar/src/App.js
+++ fullcalendar/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
 const App = () => {
+  const [schedule, setSchedules] = useState([]);
+  useEffect(() => {
+    setSchedules([
+      { title: "event 1", date: "2023-10-12" },
+      { title: "event 2", date: "2023-10-02" },
+    ]);
+  }, []);
+  console.log(schedule);
+
   return (
     <>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        weekends={false}
+        events={schedule}
+      />
     </>
   );
 };
